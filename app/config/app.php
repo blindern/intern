8a02eb64481e56a59bc2a69de5e5815c96d66ffc
Henrik Steen <henrist@henrist.net>
2013-12-22 06:13:59 +0100

    Moved url from intern2 to intern.

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://blindern-studenterhjem.no/intern2',
+	'url' => 'http://blindern-studenterhjem.no/intern',
 
 	/*
 	|--------------------------------------------------------------------------
