21326b8e5311101c22098564af199d95166379e6
Henrik Steen <henrist@henrist.net>
2014-02-05 03:02:54 +0100

    Tidssone til Oslo. Flere seeds i happeningsseeder.

--- app/database/seeds/HappeningSeeder.php
+++ app/database/seeds/HappeningSeeder.php
@@ -3,6 +3,9 @@
 class HappeningSeeder extends Seeder {
 	public function run()
 	{
+		Happening::truncate();
+		date_default_timezone_set("Europe/Oslo");
+
 		$h = new Happening();
 		$h->title = 'Juleverksted';
 		$h->by = 'Pigefaarsamlingen';
@@ -32,7 +35,34 @@ class HappeningSeeder extends Seeder {
 		$h->by = 'Hyttestyret';
 		$h->place = 'Småbruket';
 		$h->start = '2013-12-30';
-		$h->end = '2014-01-03';
+		$h->end = '2014-01-02';
+		$h->save();
+
+		$h = new Happening();
+		$h->title = 'Uoffisiell velkommen hjem-fest';
+		$h->by = 'Festforeningen';
+		$h->place = 'Billa';
+		$h->start = '2014-01-08';
+		$h->end = '2014-01-08';
+		$h->save();
+
+		$h = new Happening();
+		$h->title = 'Offisiell velkommen hjem-fest';
+		$h->by = 'Festforeningen';
+		$h->place = 'Billa';
+		$h->start = '2014-01-11';
+		$h->end = '2014-01-11';
+		$h->save();
+
+		$h = new Happening();
+		$h->title = 'Onsdagsbilla';
+		$h->by = 'Festforeningen';
+		$h->place = 'Billa';
+		$h->allday = false;
+		$h->start = new \DateTime('2014-01-15 21:00');
+		$h->end = new \DateTime('2014-01-16 01:00');
+		$h->frequency = 'WEEKLY';
+		$h->count = 18;
 		$h->save();
 	}
 }
