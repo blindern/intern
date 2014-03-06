311dc19212c1e9d848900f14540bfc15f90fd9a1
Henrik Steen <henrist@henrist.net>
2014-03-06 19:09:02 +0100

    Skrevet om faktureringsviews slik at de bruker felles data. Forbedret templatene for fakturering.

--- public/js/views/printer.fakturere.js
+++ public/js/views/printer.fakturere.js
@@ -19,7 +19,11 @@ bs.views.PrinterFakturere = bs.views.BaseView.extend({
 		}));
 
 		this.subel = this.$("#fakturere_data");
-		this.setSummed(true);
+		this.subv = new bs.views.PrinterFakturereList({
+			el: this.subel
+		});
+		this.subv.par = this;
+
 		this.dateChanged();
 	},
 
@@ -52,21 +56,15 @@ bs.views.PrinterFakturere = bs.views.BaseView.extend({
 		});
 	},
 
-	setSummed: function(dont_render)
+	setSummed: function()
 	{
-		this.subv = new bs.views.PrinterFakturereSummed({
-			el: this.subel
-		});
-		this.subv.par = this;
-		if (!dont_render) this.subv.render();
+		this.subv.view = 'printer.fakturere.summed';
+		this.subv.render();
 	},
 
 	setDetailed: function()
 	{
-		this.subv = new bs.views.PrinterFakturereDetailed({
-			el: this.subel
-		});
-		this.subv.par = this;
+		this.subv.view = 'printer.fakturere.detailed';
 		this.subv.render();
 	},
 
@@ -76,108 +74,12 @@ bs.views.PrinterFakturere = bs.views.BaseView.extend({
 	}
 });
 
-bs.views.PrinterFakturereSummed = bs.views.BaseView.extend({
-	view: 'printer.fakturere.summed',
-
-	render: function()
-	{
-		// base structure for view
-		var groups = {};
-		$(["beboer", "other"]).each(function(i, el)
-		{
-			groups[el] = {
-				is_beboer: el == "beboer",
-				printergroups: [],
-				sum_pages: 0,
-				sum_cash: 0,
-				alt_cash: false,
-				sum_cash_alt: 0
-			};
-		});
-		var total_amount = 0;
-
-		var self = this;
-		this.par.collection.each(function(printer) {
-			var section = groups[printer.get("printername") == "beboer" ? "beboer" : "other"];
-			
-			var p = {
-				users: [],
-				printername: printer.get("printername"),
-				amount: 0,
-				comment: self.par.collection.texts[printer.get("printername")],
-				sum_cash_alt: 0,
-				is_comment: false,
-				costlist: {}
-			};
-
-			printer.users.each(function(u) {
-				var amount = 0;
-				var costlist = {};
-				$(u.get("prints")).each(function(i, print)
-				{
-					amount += print.cost_each * print.sum_jobsize;
-					section.sum_pages += print.sum_jobsize;
-
-					if (!costlist[print.cost_each]) costlist[print.cost_each] = 0;
-					costlist[print.cost_each] += print.sum_jobsize;
-
-					if (!p.costlist[print.cost_each]) p.costlist[print.cost_each] = 0;
-					p.costlist[print.cost_each] += print.sum_jobsize;
-				});
-
-				if ($.inArray(p.printername, self.par.collection.no_faktura) != -1)
-				{
-					section.sum_cash_alt += amount;
-					p.sum_cash_alt += amount;
-				}
-				else
-				{
-					section.sum_cash += amount;
-					p.amount += amount;
-				}
-
-				p.users.push({
-					"realname": self.par.collection.realnames[u.get("username")],
-					"utflyttet": $.inArray(u.get("username"), self.par.collection.utflyttet) != -1,
-					"amount": amount,
-					"costlist": costlist
-				});
-			});
-
-			if (p.sum_cash_alt > 0)
-			{
-				p.alt_cash = true;
-				section.alt_cash = true;
-			}
-			total_amount += p.amount;
-
-			if (p.comment || p.sum_cash_alt > 0) p.is_comment = true;
-			section.printergroups.push(p);
-		});
-
-		this.$el.html(this.template({
-			from: this.par.collection.fromDate,
-			to: this.par.collection.toDate,
-			sum_cash: total_amount
-		}));
-
-		var self = this;
-		$(["beboer", "other"]).each(function(i, el)
-		{
-			self.$("#printer_group_"+el).html(self.getTemplate('printer.fakturere.summed_column')({
-				data: groups[el]
-			}));
-		});
-	}
-});
-
-bs.views.PrinterFakturereDetailed = bs.views.BaseView.extend({
-	view: 'printer.fakturere.detailed',
+bs.views.PrinterFakturereList = bs.views.BaseView.extend({
+	view: 'printer.fakturere.summed', // is changed by parent
 
 	render: function()
 	{
 		var data = this.getData();
-
 		this.$el.html(this.template({
 			from: this.par.collection.fromDate,
 			to: this.par.collection.toDate,
@@ -196,6 +98,7 @@ bs.views.PrinterFakturereDetailed = bs.views.BaseView.extend({
 			this.amount = 0;
 			this.amount_real = 0;
 			this.amount_alt = 0;
+			this.costlist = {};
 			this.prev = prev;
 			this.push = function(row, is_alt)
 			{
@@ -205,22 +108,34 @@ bs.views.PrinterFakturereDetailed = bs.views.BaseView.extend({
 				this[(!is_alt ? 'amount_real' : 'amount_alt')] += row.cost_each * row.sum_jobsize;
 				this[(!is_alt ? 'num_pages_real' : 'num_pages_alt')] += row.sum_jobsize;
 
+				if (!this.costlist[row.cost_each]) this.costlist[row.cost_each] = 0;
+				this.costlist[row.cost_each] += row.sum_jobsize;
+
 				if (prev) prev.push(row, is_alt);
 			};
 		};
 
-		var printers = [];
 		var totals = new summer();
+		var sections = {};
+		$(["beboer", "other"]).each(function(i, el)
+		{
+			sections[el] = {
+				is_beboer: el == "beboer",
+				printergroups: [],
+				totals: new summer(totals)
+			};
+		});
 
 		var self = this;
 		this.par.collection.each(function(printer) {
+			var section = sections[printer.get("printername") == "beboer" ? "beboer" : "other"];
 			var p = {
 				printername: printer.get("printername"),
 				comment: self.par.collection.texts[printer.get("printername")],
 				is_beboer: printer.get("printername") == "beboer",
 				users: []
 			};
-			var totals_p = new summer(totals);
+			var totals_p = new summer(section.totals);
 			var is_alt = $.inArray(p.printername, self.par.collection.no_faktura) != -1;
 
 			printer.users.each(function(u) {
@@ -247,10 +162,11 @@ bs.views.PrinterFakturereDetailed = bs.views.BaseView.extend({
 			});
 
 			$.extend(p, totals_p);
-			printers.push(p);
+			p.is_comment_or_alt = p.comment || p.amount_alt;
+			section.printergroups.push(p);
 		});
 
-		totals.printergroups = printers;
+		totals.sections = sections;
 		return totals;
 	}
 });
