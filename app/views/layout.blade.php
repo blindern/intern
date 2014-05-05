4f0a5bfdc8d4acc6efd262c2f658fd8a9f06ef4b
Henrik Steen <henrist@henrist.net>
2014-05-05 14:50:53 +0200

    Endret tekst i footer

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -105,7 +105,7 @@ if ($user)
 
     <div id="footer" class="hidden-print">
       <div class="container">
-        <p class="text-muted credit"><a href="/">Blindern Studenterhjem</a> - Kontakt <a href="/administrasjonen/om_nettsidene">Henrik Steen</a> ved henvendelser vedr. denne siden - <a href="https://github.com/blindern/intern">GitHub-prosjekt</a></p>
+        <p class="text-muted credit"><a href="/">Foreningen Blindern Studenterhjem</a> - Kontakt <a href="/administrasjonen/om_nettsidene">Henrik Steen</a> ved henvendelser vedr. denne siden - <a href="https://github.com/blindern/intern">GitHub-prosjekt</a></p>
       </div>
     </div>
   </body>
