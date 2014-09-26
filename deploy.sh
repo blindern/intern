e4d8a77f79f29e44549287904fe82c743dead07f
Henrik Steen <henrist@henrist.net>
2014-09-26 02:31:01 +0200

    Enkelt script for å kunne kjøre nødvendige prosesser ved oppdatering.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script can be used to deploy the
+# application
+
+# The script will load dependencies and
+# compile CSS/JS etc.
+
+#git pull
+composer install
+npm install
+bower install
+grunt prod
