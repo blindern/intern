06c231ad9925b882df5db4a3e83369266173acf7
Henrik Steen <henrist@henrist.net>
2014-11-02 09:01:29 +0100

    Frontend for bokdatabase oppdatert. Nå full fungerende løsning for registrering av bøker. Oppdatert bokliste og infoside for hver bok.

--- app/assets/javascript/books.js
+++ app/assets/javascript/books.js
@@ -22,14 +22,25 @@ mod.config(['$routeProvider', function($routeProvider) {
         });
 }]);
 
-mod.service('Book', function($resource) {
-    return $resource('api/books/:id', {id: '@_id'}, {
+mod.service('Book', function($resource, $http) {
+    var Book = $resource('api/books/:id', {id: '@_id'}, {
         query: { method: 'GET', isArray: false }
     });
+
+    Book.prototype.setBarcode = function(barcode) {
+        var self = this;
+        return $http.post('api/books/'+this._id+'/barcode', {
+            'barcode': barcode
+        }).success(function() {
+            self.bib_barcode = barcode;
+        });
+    };
+
+    return Book;
 });
 
 mod.controller('BookHomeCtrl', function(Page, Book, $scope) {
-	Page.setTitle('Bokdatabase');
+	Page.setTitle('Biblioteket på Blindern Studenterhjem');
 
     $scope.currentPage = 1;
 
@@ -73,27 +84,56 @@ mod.controller('BookHomeCtrl', function(Page, Book, $scope) {
     pageChange();
 });
 
-mod.controller('BookRegisterCtrl', function(Page, Book, $scope, $http, $location) {
+mod.controller('BookRegisterCtrl', function(Page, Book, AuthService, $scope, $http, $location) {
     Page.setTitle('Registrer bok');
 
+    if (!AuthService.requireGroup('biblioteksutvalget')) {
+        $scope.noaccess = true;
+        return;
+    }
+
+    $scope.addmore = true;
     $scope.book = new Book();
 
+    // restore last used parameters
+    function restoreParams() {
+        if (typeof(Storage) !== 'undefined') {
+            $scope.book.bib_room = sessionStorage.bookRoom || 'Biblioteket';
+            $scope.book.bib_section = sessionStorage.bookSection || '';
+        }
+    }
+    restoreParams();
+
     $scope.isbnSearch = function() {
-        console.log("search for isbn", $scope.isbn_search);
+        if (!$scope.book.isbn) return;
+        $scope.book.isbn = $scope.book.isbn.replace(/-/g, "");
+        $scope.isbn_is_searching = true;
+        console.log("search for isbn", $scope.book.isbn);
         $http.post('api/books/isbn', {
-            'isbn': $scope.isbn_search
+            'isbn': $scope.book.isbn
         }).success(function(data) {
             console.log("isbn got data back", data);
             $scope.book = new Book();
             $scope.book.isbn = data.isbn;
+
+            restoreParams();
+
             angular.forEach(data.data, function(value, key) {
                 $scope.book[key] = value;
             });
             document.getElementById('title').focus();
+        }).then(function() {
+            $scope.isbn_is_searching = false;
         });
     };
 
     $scope.addBook = function() {
+        // save some paramters for later usage
+        if (typeof(Storage) !== 'undefined') {
+            sessionStorage.bookRoom = $scope.book.bib_room;
+            sessionStorage.bookSection = $scope.book.bib_section;
+        }
+
         $scope.book.$save(function(result) {
             console.log("book saved?", result);
             if (result._id) {
@@ -103,14 +143,45 @@ mod.controller('BookRegisterCtrl', function(Page, Book, $scope, $http, $location
     };
 });
 
-mod.controller('BookItemCtrl', function(Page, Book, $scope, $routeParams) {
+mod.controller('BookItemCtrl', function(Page, Book, AuthService, $scope, $routeParams, $location) {
     Page.setTitle("Laster bok..");
 
-    console.log("got id?", $routeParams.id);
-
     Book.get({'id': $routeParams.id}, function(result) {
-        console.log("found book?", result);
         $scope.book = result;
-        Page.setTitle(result.title);
+        Page.setTitle(result.title + (result.subtitle ? ': ' + result.subtitle : ''));
+
+        var handleBarcode = function() {
+            $scope.bib_barcode = $scope.book.bib_barcode ? $scope.book.bib_barcode/*.substring(0, $scope.book.bib_barcode.length - 3)*/ : null;
+
+            if (!$scope.book.bib_barcode) {
+                $scope.show_barcode_form = AuthService.inGroup('biblioteksutvalget');
+            }
+        };
+        $scope.$watch('book.bib_barcode', handleBarcode);
+        handleBarcode();
+    }, function() {
+        // send to book index on error
+        $location.path('books');
     });
+
+    $scope.deleteBook = function() {
+        if (confirm("Er du sikker på at du vil slette boka fra databasen?")) {
+            $scope.book.$delete(function() {
+                $location.path('books');
+            });
+        }
+    };
+
+    $scope.editBook = function() {
+        // TODO
+        alert("Uferdig funksjon!");
+    }
+
+    $scope.registerBarcode = function() {
+        if ($scope.barcode.substring(0, 3) != "BS-") {
+            alert("Ugyldig strekkode for biblioteket.");
+        } else {
+            $scope.book.setBarcode($scope.barcode);
+        }
+    };
 });
