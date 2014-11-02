5a2000db39f4bf38802d3f73b2e6da4e2a054f33
Henrik Steen <henrist@henrist.net>
2014-11-02 09:08:39 +0100

    Endrer timeout på auto-focus directive for å løse at de ikke alltid fungerte.

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -58,9 +58,9 @@ module.directive('autoFocus', function($timeout) {
     return {
         restrict: 'AC',
         link: function(_scope, _element) {
-            $timeout(function(){
+        	$timeout(function(){
                 _element[0].focus();
-            }, 0);
+            }, 100);
         }
     };
 });
