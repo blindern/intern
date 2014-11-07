ca07becd23aee44d6b440d677923cbb118f64c39
Henrik Steen <henrist@henrist.net>
2014-11-07 09:24:06 +0100

    Dagens og morgendagens rett på forsiden.

--- app/assets/javascript/index.js
+++ app/assets/javascript/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('intern.index', ['ngRoute', 'intern.helper.page'])
+angular.module('intern.index', ['ngRoute', 'intern.helper.page', 'intern.matmeny'])
 
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
@@ -9,6 +9,25 @@ angular.module('intern.index', ['ngRoute', 'intern.helper.page'])
 	});
 }])
 
-.controller('IndexCtrl', function(Page) {
+.controller('IndexCtrl', function(Page, Matmeny, $http, $scope) {
 	Page.setTitle('Foreningen Blindern Studenterhjem');
+
+    // hent matmeny
+    var today = moment().format('YYYY-MM-DD');
+    var tomorrow = moment().add(1, 'days').format('YYYY-MM-DD');
+    Matmeny.query({'from': today, 'to': tomorrow}, function(ret) {
+        var items = {};
+        angular.forEach(ret, function(item) {
+            items[item.day] = item;
+        });
+
+        var m = {};
+        if (items[today]) m.today = items[today];
+        if (items[tomorrow]) m.tomorrow = items[tomorrow];
+        $scope.matmeny = m;
+        $scope.matmenydate = {
+            today: today,
+            tomorrow: tomorrow
+        };
+    });
 });
