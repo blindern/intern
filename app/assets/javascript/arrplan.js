a3d9baf7df63f41783db7bc13602e6b6cdc3f170
Henrik Steen <henrist@henrist.net>
2014-11-15 21:12:47 +0100

    Bruker momentjs for å hente dato for arrangementer. I Firefox fungerte ikke den gamle metoden.

--- app/assets/javascript/arrplan.js
+++ app/assets/javascript/arrplan.js
@@ -60,20 +60,17 @@ angular.module('intern.arrplan', ['ngRoute', 'intern.helper.page'])
 		getSem: function(arrevent) {
 			var t = function(d)
 			{
-				var x = (d.getMonth() >= 6 ? "h" : "v");
-				return x + d.getFullYear().toString().substr(2, 2);
+				var x = (d.month() >= 6 ? "h" : "v");
+				return x + d.year().toString().substr(2, 2);
 			};
 
 			if (arrevent.type == "comment")
 			{
-				return [t(new Date(arrevent.date))];
+				return [t(moment(arrevent.date))];
 			}
-			
-			var t1 = new Date(arrevent.start);
-			var t2 = new Date(arrevent.end);
 
-			var s1 = t(t1);
-			var s2 = t(t2);
+			var s1 = t(moment(arrevent.start));
+			var s2 = t(moment(arrevent.end));
 
 			var r = [s1];
 			if (s2 != s1) r.push(s2);
