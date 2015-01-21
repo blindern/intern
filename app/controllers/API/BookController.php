5f743d5bb2837a263d548501ad83dbc766c073ea
Henrik Steen <henrist@henrist.net>
2015-01-21 18:10:49 +0100

    Fix bug in pubdate parser

--- app/controllers/API/BookController.php
+++ app/controllers/API/BookController.php
@@ -60,7 +60,7 @@ class BookController extends \Controller {
             'title' => 'required',
             'subtitle' => '',
             'authors' => 'array',
-            'pubdate' => array('regex:/^(\d{4}-\d\d(-\d\d)?|\d{4}\?|\d{2}\?)$/'),
+            'pubdate' => array('regex:/^(\d{4}-\d\d(-\d\d)?|\d{4}\??|\d{2}\?)$/'),
             'description' => '',
             'isbn' => '',
             'bib_comment' => '',
