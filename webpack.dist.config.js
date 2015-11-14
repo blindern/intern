fdcc681fb7c50f58853ab4eee571aafbdba2d6fa
Henrik Steen <henrist@henrist.net>
2015-11-14 17:49:26 +0100

    Ikke minifiser HTML

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -34,7 +34,7 @@ module.exports = {
       {test: /\.scss$/, loader: 'style!css!sass'},
       {test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
       {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
-      {test: /\.html$/, loader: 'ngtemplate?module=intern&relativeTo=' + (path.resolve(__dirname, './')) + '/!html'},
+      {test: /\.html$/, loader: 'ngtemplate?module=intern&relativeTo=' + (path.resolve(__dirname, './')) + '/!html?minimize=false'},
       {test: /\.json$/, loader: 'json'},
     ]
   },
