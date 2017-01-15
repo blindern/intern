7b3f66e536f4d62ba47c96f785a958982bfbf544
Henrik Steen <henrist@henrist.net>
2017-01-15 19:06:12 +0100

    Remove unused database options

--- config/database.php
+++ config/database.php
@@ -46,44 +46,6 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver'   => 'sqlite',
-            'database' => storage_path().'/database.sqlite',
-            'prefix'   => '',
-        ],
-
-        'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_MYSQL_HOST', 'localhost'),
-            'database'  => env('DB_MYSQL_DATABASE', 'forge'),
-            'username'  => env('DB_MYSQL_USERNAME', 'forge'),
-            'password'  => env('DB_MYSQL_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
-
-        'pgsql' => [
-            'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-        ],
-
-        'sqlsrv' => [
-            'driver'   => 'sqlsrv',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'prefix'   => '',
-        ],
-
         'mongodb' => [
             'driver'   => 'mongodb',
             'host'     => env('DB_MONGODB_HOST', 'localhost'),
