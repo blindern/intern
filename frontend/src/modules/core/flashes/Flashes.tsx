302ff998e203cde87f4d836ff0b4530f5368ca3f
Henrik Steen <henrist@henrist.net>
2018-12-31 08:31:49 +0100

    Add styled-components and animate flashes

--- frontend/src/modules/core/flashes/Flashes.tsx
+++ frontend/src/modules/core/flashes/Flashes.tsx
@@ -1,27 +1,62 @@
-import classNames from 'classnames'
 import React, { useContext } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import styled, { keyframes } from 'styled-components'
 import { formatDate } from 'utils/dates'
-import './Flashes.scss'
 import { FlashesContext } from './FlashesProvider'
+
+const animateIn = keyframes`
+  from {
+    opacity: 1;
+    max-height: 0;
+  }
+  to {
+    max-height: 100px;
+  }
+`
+
+const animateOut = keyframes`
+  from {
+    opacity: 1;
+    max-height: 100px;
+  }
+  to {
+    opacity: 0;
+    max-height: 0;
+  }
+`
+
+const MessageBoxWrap = styled.div`
+  overflow-y: hidden;
+  box-sizing: border-box;
+
+  &.flash-enter {
+    animation: ${animateIn} 0.2s forwards;
+  }
+  &.flash-exit {
+    animation: ${animateOut} 0.2s forwards;
+  }
+`
+
+const MessageBox = styled.div`
+  margin-top: 15px;
+  padding: 15px;
+`
 
 const Flashes = () => {
   const { flashes } = useContext(FlashesContext)
 
   return (
-    <>
+    <TransitionGroup component={null}>
       {flashes.map((flash, idx) => (
-        <div key={idx} className='message-box-wrap'>
-          <div
-            className={classNames(
-              'message-box',
-              flash.type ? 'bg-' + flash.type : null,
-            )}
-          >
-            <b>{formatDate(flash.date, 'HH:mm:ss')}:</b> {flash.message}
-          </div>
-        </div>
+        <CSSTransition timeout={300} classNames='flash' key={idx}>
+          <MessageBoxWrap>
+            <MessageBox className={flash.type ? `bg-${flash.type}` : ''}>
+              <b>{formatDate(flash.date, 'HH:mm:ss')}:</b> {flash.message}
+            </MessageBox>
+          </MessageBoxWrap>
+        </CSSTransition>
       ))}
-    </>
+    </TransitionGroup>
   )
 }
 
