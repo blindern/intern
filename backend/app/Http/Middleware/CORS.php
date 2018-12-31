894fb5c7e5503263a4024a4f267de13eda0e5f33
Henrik Steen <henrist@henrist.net>
2018-12-31 11:34:12 +0100

    Extend CORS setup

--- backend/app/Http/Middleware/CORS.php
+++ backend/app/Http/Middleware/CORS.php
@@ -1,9 +1,19 @@
 <?php namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Foundation\Http\Events\RequestHandled;
 use Closure;
 
 class CORS
 {
+    /** @var Dispatcher $events */
+    protected $events;
+
+    public function __construct(Dispatcher $events)
+    {
+        $this->events = $events;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -13,11 +23,30 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        $this->events->listen(RequestHandled::class, function (RequestHandled $event) {
+            $this->addHeaders($event->request, $event->response);
+        });
 
-        // give access to the phone app testing page
-        $response->headers->set('Access-Control-Allow-Origin', 'http://dev1.hsw.no:8000');
+        /*
+        $this->events->listen('kernel.handled', function (\Illuminate\Http\Request $request, \Illuminate\Http\Response $response) {
+            $this->addHeaders($request, $response);
+        });
+        */
+
+        $response = $request->getMethod() === 'OPTIONS'
+            ? new \Illuminate\Http\Response()
+            : $next($request);
+
+        $this->addHeaders($request, $response);
 
         return $response;
+    }
+
+    protected function addHeaders($request, $response)
+    {
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Allow-Headers', 'content-type, x-xsrf-token, x-csrf-token');
+        $response->headers->set('Access-Control-Expose-Headers', 'x-flashes');
     }
 }
