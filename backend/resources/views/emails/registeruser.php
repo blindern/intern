e4a01b6ee4102bdb30c6aaae436a4c3ad4e863c5
Henrik Steen <henrist@henrist.net>
2022-07-13 21:27:13 +0200

    Improve registeruser template

--- backend/resources/views/emails/registeruser.php
+++ backend/resources/views/emails/registeruser.php
@@ -3,17 +3,20 @@
 
 Info til IT-gruppa:
 
-Fornavn: "<?=$firstname?>"
-Etternavn: "<?=$lastname?>"
-E-post: "<?=$email?>"
-Ønsket brukernavn: "<?=$username?>"
-Mobilnr: <?=($phone ? "\"{$phone}\"" : "ikke registrert")?>
+Fornavn: "<?= $firstname ?>"
+Etternavn: "<?= $lastname ?>"
+E-post: "<?= $email ?>"
+Ønsket brukernavn: "<?= $username ?>"
+Mobilnr: <?= ($phone ? "\"{$phone}\"" : "ikke registrert") ?>
 
 
 Kommando for å opprette:
-/fbs/drift/nybruker/process.sh <?=$filename?>
+/fbs/drift/nybruker/process.sh <?= $filename ?>
 
 
-Sendt fra <?=$_SERVER['REMOTE_ADDR']?>
+Nærmere tekniske detaljer kan sees i https://github.com/blindern/drift/tree/main/ldap-toolbox
 
-<?=$_SERVER['HTTP_USER_AGENT']?>
+
+Sendt fra <?= isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'] ?>
+
+<?= $_SERVER['HTTP_USER_AGENT'] ?>
