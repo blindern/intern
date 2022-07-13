8db4346e4c49ed0af109cf5399fcd8b810ab736d
Henrik Steen <henrist@henrist.net>
2022-07-13 12:40:06 +0200

    Setup TrustProxies

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
